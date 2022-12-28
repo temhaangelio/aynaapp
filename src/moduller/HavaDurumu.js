5e625bb5fbdcbdcecf594c5e3b95ac9cc247c7e7
temhaangelio <temha@AHMET-MacBook-Pro.local>
2022-12-28 14:04:36 +0300

    son

--- src/moduller/HavaDurumu.js
+++ src/moduller/HavaDurumu.js
@@ -5,23 +5,34 @@ const HavaDurumu = () => {
   const [hava, setHava] = useState([]);
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      setHava([...hava, res.data]);
-    });
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/onecall?lat=40.193298&lon=29.074202&exclude=current,hourly,minutely,alerts&units=metric&lang=tr&appid=" +
+          process.env.REACT_APP_HAVADURUMU
+      )
+      .then((res) => {
+        setHava((current) => [...hava, ...res.data.daily]);
+      });
   }, []);
 
   return (
     <div id="havadurumu">
-      <h1>23°</h1>
+      <h1>{hava[0]?.temp.day.toFixed(0)}°</h1>
       <div className="gunler">
-        <div>
-          <div>Salı</div>
-          <div>13°/6°</div>
-        </div>
-        <div>
-          <div>Salı</div>
-          <div>13°/6°</div>
-        </div>
+        {hava.map((gun, index) => {
+          if (index < 5) {
+            return (
+              <div className="gun" key={gun.temp.day}>
+                <h1>{gun.temp.day.toFixed(0)}°</h1>
+                <span>
+                  {new Date(gun.dt * 1000).toLocaleDateString("tr", {
+                    weekday: "long",
+                  })}
+                </span>
+              </div>
+            );
+          }
+        })}
       </div>
     </div>
   );
